7097ab79bc8d42014116232fa3b9bae5a5aa2619
Daniel Jeba Raj <danieljr.programmer@gmail.com>
2022-12-03 08:10:00 +0530

    [Module-4]Assignment-1

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+//import { ServerComponent } from './server.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
